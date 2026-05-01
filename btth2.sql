f0cf357d2b0dc94f700106298a80d7111452823b
Nguyễn Hoàng Quân <nguyenhoangquan7907@gmail.com>
2026-05-01 14:44:53 +0700

    btth2 1/5/2026

--- btth2.sql
+++ btth2.sql
@@ -30,3 +30,23 @@ create table bookings(
     phone varchar(11) not null,
     booking_date datetime
 );
+
+INSERT INTO movies VALUES 
+('M01', 'Dune: Part Two', 166, 13), ('M02', 'The Batman II', 175, 13),
+('M03', 'Deadpool & Wolverine', 127, 18), ('M04', 'Kung Fu Panda 4', 94, 0);
+
+INSERT INTO rooms VALUES 
+('R01', 'Cinema 01', 150, 'active'), ('R02', 'Cinema 02', 120, 'active'),
+('R03', 'IMAX Premium', 80, 'maintenance');
+
+INSERT INTO showtimes VALUES 
+('S01', 'M01', 'R01', '2026-05-01 18:00', 120000), ('S02', 'M03', 'R02', '2026-05-01 20:30', 150000),
+('S03', 'M04', 'R01', '2026-05-02 09:00', 90000), ('S04', 'M02', 'R02', '2026-05-02 14:00', 130000),
+('S05', 'M01', 'R01', '2026-05-02 21:00', 120000);
+
+INSERT INTO bookings VALUES 
+('B01', 'S01', 'An', '0901234567', NOW()), ('B02', 'S01', 'Bình', '0907654321', NOW()),
+('B03', 'S02', 'Chi', '0912345678', NOW()), ('B04', 'S02', 'Duy', '0922334455', NOW()),
+('B05', 'S03', 'Hoa', '0933445566', NOW()), ('B06', 'S03', 'Khải', '0944556677', NOW()),
+('B07', 'S04', 'Linh', '0955667788', NOW()), ('B08', 'S04', 'Minh', '0966778899', NOW()),
+('B09', 'S05', 'Nam', '0977889900', NOW()), ('B10', 'S05', 'Oanh', '0988990011', NOW());
