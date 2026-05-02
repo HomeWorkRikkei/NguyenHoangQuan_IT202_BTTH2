58f699c5b32e3e755ebb0646e7002a1b1911ced0
Nguyễn Hoàng Quân <nguyenhoangquan7907@gmail.com>
2026-05-02 20:52:17 +0700

    btth 2/5/2026

--- btth2.sql
+++ btth2.sql
@@ -37,7 +37,7 @@ INSERT INTO movies VALUES
 
 INSERT INTO rooms VALUES 
 ('R01', 'Cinema 01', 150, 'active'), ('R02', 'Cinema 02', 120, 'active'),
-('R03', 'IMAX Premium', 80, 'maintenance');
+('R03', 'IMAX Premium', 80, 'active');
 
 INSERT INTO showtimes VALUES 
 ('S01', 'M01', 'R01', '2026-05-01 18:00', 120000), ('S02', 'M03', 'R02', '2026-05-01 20:30', 150000),
@@ -48,5 +48,28 @@ INSERT INTO bookings VALUES
 ('B01', 'S01', 'An', '0901234567', NOW()), ('B02', 'S01', 'Bình', '0907654321', NOW()),
 ('B03', 'S02', 'Chi', '0912345678', NOW()), ('B04', 'S02', 'Duy', '0922334455', NOW()),
 ('B05', 'S03', 'Hoa', '0933445566', NOW()), ('B06', 'S03', 'Khải', '0944556677', NOW()),
-('B07', 'S04', 'Linh', '0955667788', NOW()), ('B08', 'S04', 'Minh', '0966778899', NOW()),
+('B07', 'S04', 'Linh', '0987654321', NOW()), ('B08', 'S04', 'Minh', '0966778899', NOW()),
+('B11', 'S04', 'Linh', '0987654321', NOW()),('B12', 'S04', 'Linh', '0987654321', NOW()),
 ('B09', 'S05', 'Nam', '0977889900', NOW()), ('B10', 'S05', 'Oanh', '0988990011', NOW());
+
+SET SQL_SAFE_UPDATES = 0;
+
+update rooms set status='maintenance' where id='R01';
+select * from rooms;
+
+update showtimes set room_id='R02' where room_id='R01';
+select * from showtimes;
+
+delete from bookings where phone='0987654321';
+select * from bookings;
+
+DELETE FROM bookings 
+WHERE showtime_id IN (SELECT id FROM showtimes WHERE movie_id = 'M03');
+
+DELETE FROM showtimes 
+WHERE movie_id = 'M03';
+
+DELETE FROM movies 
+WHERE id = 'M03';
+
+SET SQL_SAFE_UPDATES = 1;
